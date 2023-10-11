6429cf74eaa80023fe82f5db06e5d687d616bc74
DanTestehan <tdan89@yahoo.com>
2023-10-11 08:28:17 +0300

    application properties and unused tests file

---
+++ src/test/java/com/testehan/SpringBootDockerComposeExperiments/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testehan.SpringBootDockerComposeExperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
